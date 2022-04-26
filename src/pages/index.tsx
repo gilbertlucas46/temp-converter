ac71693f84a1b3ea17d5f9b4dddc0b9ef5c32fb9
Gilbert Lucas <lucas@gilbert.codes>
2022-04-26 21:21:24 +0800

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import * as React from "react"
+
+// styles
+const pageStyles = {
+  
+}
+
+// markup
+const IndexPage = () => {
+  return (
+    <main style={pageStyles}>
+      <title>Home Page</title>
+     
+    </main>
+  )
+}
+
+export default IndexPage
