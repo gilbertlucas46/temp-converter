84aa2c0814475334403270817dbc2477fdc1af7d
Gilbert Lucas <lucas@gilbert.codes>
2022-04-26 21:31:10 +0800

    added react helmet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,18 @@
-import * as React from "react"
+import * as React from "react";
+import {Helmet} from "react-helmet";
 
 // styles
 const pageStyles = {
-  
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
 }
 
 // markup
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
-      <title>Home Page</title>
+     <Helmet title="Temperature Converter" defer={false} />
      
     </main>
   )
