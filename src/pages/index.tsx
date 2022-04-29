2e785472eedeab896601d75f294b4d3981903811
Gilbert Lucas <lucas@gilbert.codes>
2022-04-29 20:37:36 +0800

    removed imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import {Helmet} from "react-helmet"; // To modify head on each pages
 import Layout from "./layout";
 import { TemperatureInput } from "../components/TemperatureInput";
-import { TempZones } from "../components/TempZones";
 import styled, { ThemeProvider } from 'styled-components'
 import DividerStyles from '../components/styles/DividerStyles.js'
 import Wave from '../images/wave.svg';
