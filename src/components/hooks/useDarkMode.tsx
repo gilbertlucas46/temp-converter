5cef746319f1e06921e83bfb30862beb33ae9442
Gilbert Lucas <lucas@gilbert.codes>
2022-04-29 23:22:24 +0800

    additional

--- src/components/hooks/useDarkMode.tsx
+++ src/components/hooks/useDarkMode.tsx
@@ -2,24 +2,24 @@ import { useEffect, useState } from "react";
 
 export const useDarkMode = () => {
     const [theme, setTheme] = useState('light');
-  
+
     const toggleTheme = () => {
         if (theme === 'light') {
             setTheme('dark')
-            window.localStorage.setItem('theme', 'dark');
+            window?.localStorage.setItem('theme', 'dark');
         } else {
             setTheme('light')
-            window.localStorage.setItem('theme', 'light');
+            window?.localStorage.setItem('theme', 'light');
         }
     };
     
     useEffect(() => {
-        const localTheme = window.localStorage.getItem('theme');
+        const localTheme = window?.localStorage.getItem('theme');
     
         if (localTheme) {
             setTheme(localTheme);
         } else {
-            window.localStorage.setItem('theme', 'light');
+            window?.localStorage.setItem('theme', 'light');
         }
     })
   
